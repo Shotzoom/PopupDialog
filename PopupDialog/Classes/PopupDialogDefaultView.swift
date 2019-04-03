5f140aff5b1204aa96cf8f0089f4eb7d4076168c
Rashaad Sidique <rsidique@gmail.com>
2019-04-03 15:14:35 -0700

    Fix image view content mode

--- PopupDialog/Classes/PopupDialogDefaultView.swift
+++ PopupDialog/Classes/PopupDialogDefaultView.swift
@@ -88,7 +88,7 @@ final public class PopupDialogDefaultView: UIView {
     internal lazy var imageView: UIImageView = {
         let imageView = UIImageView(frame: .zero)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleAspectFit
         imageView.clipsToBounds = true
         return imageView
     }()
